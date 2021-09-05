8aa1869e4367d8ccc332482e51bcd5267bcec1d6
bakagai <>
2021-09-04 17:23:49 -0700

    feature: add href to footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 import githubLogo from '../../images/github.svg';
@@ -14,22 +15,22 @@ function Footer() {
           <nav className="footer__nav">
             <ul className="footer__links footer__links_type_app">
               <li className="footer__link-container">
-              <a className="footer__link">Home</a>
+              <Link to="/" className="footer__link">Home</Link>
               </li>
               <li className="footer__link-container">
-              <a className="footer__link">Practicum by Yandex</a>
+              <a href="https://practicum.yandex.com/" target="_blank" className="footer__link">Practicum by Yandex</a>
               </li>
             </ul>
           </nav>
           <nav className="footer__nav">
             <ul className="footer__links">
               <li className="footer__link-container">
-                <a className="footer__link">
+                <a href="https://github.com/sayus2884" target="_blank" className="footer__link">
                 <img src={githubLogo}/>
                 </a>
               </li>
               <li className="footer__link-container">
-                <a className="footer__link">
+                <a href="https://fb.com/" target="_blank" className="footer__link">
                 <img src={fbLogo}/>
                 </a>
               </li>
